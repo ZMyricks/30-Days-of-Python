a51a0a50ea137bec8ba6b70691132419f192bf8e
ZMyricks <105020592+ZMyricks@users.noreply.github.com>
2022-09-02 23:30:25 -0700

    day 5 lists

---
+++ day_5/lists.py
@@ -0,0 +1,80 @@
+# Declare an empty list
+queens_names_empty = []
+other_list = list()
+
+# Declare a list with more than 5 items
+queens_names = ['Katya Z.', 'BeBe Zahara Benet', 'Vivianne Vanderpuss', 'Icesis Couture', 'Pangina Heels', 'Mo Heart']
+# Find the length of your list
+print(len(queens_names))
+# Get the first item, the middle item and the last item of the list
+first_queen = queens_names[0]
+last_queen = queens_names[5]
+middle_queen = queens_names[3]
+print(first_queen, middle_queen, last_queen)
+
+# Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Miz Cracker', 23, 5.5, 'single', '123 drag queen lane']
+print(mixed_data_types)
+print(type(mixed_data_types))
+
+# Print the list after modifying one of the companies
+mixed_data_types[0] = 'Blair St. Clair'
+print(mixed_data_types)
+
+# Add to the list
+mixed_data_types.append('lip sync assasin')
+print(mixed_data_types)
+
+# Insert value into the list
+mixed_data_types.insert(3, 'from the house of HALL!!')
+print(mixed_data_types)
+
+# Change one of the names to uppercase
+first_name_to_uppercase = queens_names[0].upper()
+queens_names[0] = first_name_to_uppercase
+print(queens_names)
+
+# Join the it_companies with a string '#;  '
+
+
+
+# Check if a certain company exists in the it_companies list.
+
+# Sort the list using sort() method
+
+# Reverse the list in descending order using reverse() method
+
+# Slice out the first 3 companies from the list
+
+# Slice out the last 3 companies from the list
+
+# Slice out the middle IT company or companies from the list
+
+# Remove the first IT company from the list
+
+# Remove the middle IT company or companies from the list
+
+# Remove the last IT company from the list
+
+# Remove all IT companies from the list
+
+# Destroy the IT companies list
+
+# Join the following lists:
+
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+# back_end = ['Node','Express', 'MongoDB']
+# After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack. Then insert Python and SQL after Redux.
+
+# Exercises: Level 2
+# The following is a list of 10 students ages:
+# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# Sort the list and find the min and max age
+# Add the min age and the max age again to the list
+# Find the median age (one middle item or two middle items divided by two)
+# Find the average age (sum of all items divided by their number )
+# Find the range of the ages (max minus min)
+# Compare the value of (min - average) and (max - average), use abs() method
+# Find the middle country(ies) in the countries list
+# Divide the countries list into two equal lists if it is even if not one more country for the first half.
+# ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
