6cadfde5356a8ab6ff7898f1d48bde2ea303461e
ZMyricks <105020592+ZMyricks@users.noreply.github.com>
2022-09-02 22:58:03 -0700

    update

--- day_4/strings.py
+++ day_4/strings.py
@@ -1,4 +1,7 @@
 # Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+import string
+
+
 sentence = str('Thirty ' + 'Days ' + 'Of ' + 'Python')
 print(sentence)
 print(type(sentence))
@@ -21,48 +24,69 @@ print(sentence.title())
 print(sentence.swapcase())
 
 # Cut(slice) out the first word of Coding For All string.
-print(company)
+print(company[6::])
+
 
 # Check if Coding For All string contains a word Coding using the method index, find or other methods.
+print(bool(company.find('Coding')))
+
 # Replace the word coding in the string 'Coding For All' to Python.
+print(company.replace('Coding', 'Happiness'))
+
 # Change Python for Everyone to Python for All using the replace method or other methods.
+text = 'Python for Everyone'
+new_text = text.replace('Everyone', 'All')
+print(new_text)
+
 # Split the string 'Coding For All' using space as the separator (split()) .
+new_split = company.split(' ')
+print(new_split)
+
 # "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(companies.split(','))
+
 # What is the character at index 0 in the string Coding For All.
+string_is = 'Coding For All'
+print(string_is[0])
+
 # What is the last index of the string Coding For All.
+print(string_is[-1])
+
 # What character is at index 10 in "Coding For All" string.
+print(string_is[10]) #returns space character
+
 # Create an acronym or an abbreviation for the name 'Python For Everyone'.
-# Create an acronym or an abbreviation for the name 'Coding For All'.
+phrase = "Python for Everybody"
+new_phrase = phrase.split()
+acronym = ""
+for word in new_phrase:
+  acronym = acronym + word[0].upper()
+print(acronym)
+
 # Use index to determine the position of the first occurrence of C in Coding For All.
+text = "Coding For All"
+print(text.find('C'))
+
 # Use index to determine the position of the first occurrence of F in Coding For All.
+text = "Coding For All"
+print(text.find('F'))
+
 # Use rfind to determine the position of the last occurrence of l in Coding For All People.
-# Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
-# Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
-# Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
-# Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
-# Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
-# Does ''Coding For All' start with a substring Coding?
-# Does 'Coding For All' end with a substring coding?
-# '   Coding For All      '  , remove the left and right trailing spaces in the given string.
-# Which one of the following variables return True when we use the method isidentifier():
-# 30DaysOfPython
-# thirty_days_of_python
-# The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
-# Use the new line escape sequence to separate the following sentences.
-# I am enjoying this challenge.
-# I just wonder what is next.
-# Use a tab escape sequence to write the following lines.
-# Name      Age     Country   City
-# Asabeneh  250     Finland   Helsinki
-# Use the string formatting method to display the following:
-# radius = 10
-# area = 3.14 * radius ** 2
-# The area of a circle with radius 10 is 314 meters square.
-# Make the following using string formatting methods:
-# 8 + 6 = 14
-# 8 - 6 = 2
-# 8 * 6 = 48
-# 8 / 6 = 1.33
-# 8 % 6 = 2
-# 8 // 6 = 1
-# 8 ** 6 = 262144
+text = "Coding For All"
+print(text.rfind('l'))
+
+# Use index or find to find the position of the first occurrence of
+# the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+phrase = 'You cannot end a sentence with because because because is a conjunction'
+print(phrase.index('because'))
+
+# Use rindex to find the position of the last occurrence of
+# the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+phrase = 'You cannot end a sentence with because because because is a conjunction'
+print(phrase.rindex('because'))
+
+# Slice out the phrase 'because because because' in the following
+# sentence: 'You cannot end a sentence with because because because is a conjunction'
+phrase = 'You cannot end a sentence with because because because is a conjunction'
+print(phrase.replace('because because because', ''))
